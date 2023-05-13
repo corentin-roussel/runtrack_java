152a3550c3ec19f43e14d027a953a04b13f054bd
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-13 17:58:31 +0200

    rendu job_04 declaration of three variables on one lign et initialisation of the varaible on one line each

---
+++ jour_01/job_04/DeclarationVariable.java
@@ -0,0 +1,12 @@
+package jour_01.job_04;
+
+class DeclarationVariable {
+    public static void main(String args[])
+    {
+        int one ,two ,three;
+        
+        one = 0;
+        two = 0;
+        three = 0;
+    }
+}
