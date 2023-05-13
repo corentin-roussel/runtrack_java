f4ae6eced11da55aff302cd6e8cdd60f775fd0bb
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-13 18:05:34 +0200

    declaration variable add and multiply display each time

---
+++ jour_01/job_05/Calcul.java
@@ -0,0 +1,19 @@
+package jour_01.job_05;
+
+public class Calcul {
+    public static void main(String args[]) 
+    {
+        int nombre;
+        nombre = 7;
+
+        System.out.println(nombre);
+
+        nombre = nombre + 3;
+        
+        System.out.println(nombre);
+
+        nombre = nombre * 5;
+
+        System.out.println(nombre);
+    }
+}
