4d90e52ea9b28f7cd1c2394852f4ec0fad57f92e
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-13 18:15:56 +0200

    swap variables valor

---
+++ jour_01/job_06/ExerciceVariables.java
@@ -0,0 +1,20 @@
+package jour_01.job_06;
+
+public class ExerciceVariables {
+    public static void main(String args[])
+    {
+        int num1, num2;
+
+        num1 = 5;
+        num2 = 10;
+ 
+        System.out.println(" La valeur de num1 est" + num1 +" est la valeur de num2 est " + num2);
+        
+
+        num1 = num1 ^ num2 ^ (num2 = num1);
+        
+
+        System.out.println("La valeur de num1 est " + num1 +" est la valeur de num2 est " + num2);
+        
+    }
+}
