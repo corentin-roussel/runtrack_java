bd48f917be4b8ebc9bf2186bce7c98c2e5f4e53b
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-13 18:53:47 +0200

    find four way to declare a string

---
+++ jour_01/job_09/Mystring.java
@@ -0,0 +1,20 @@
+package jour_01.job_09;
+
+public class Mystring {
+    public static void main(String args[])
+    {
+        String one = "Hello";
+
+        String two = new String("World!");
+
+        char three[] = {'I',' ','l','o','v','e'};
+
+        char four [] = {115,116,114,105,110,103,115};
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
+
+    }
+}
