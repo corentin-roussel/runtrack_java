dd390cce0395a46a5a18d703e447f923f3f12082
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-13 18:23:29 +0200

    add two values predefined

---
+++ jour_01/job_07/Addition.java
@@ -0,0 +1,13 @@
+package jour_01.job_07;
+
+public class Addition {
+    public static void main(String args[])
+    {
+        int num1, num2;
+        
+        num1 = 3;
+        num2 = 73;
+
+        System.out.println(num1 + num2);
+    }
+}
