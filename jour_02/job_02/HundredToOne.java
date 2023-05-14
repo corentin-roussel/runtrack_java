eb5797b28e39bc4a19f8ff5631980a936e10a54b
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-14 13:26:35 +0200

    use loop to display 1 to 100 and 100 to 1

---
+++ jour_02/job_02/HundredToOne.java
@@ -0,0 +1,13 @@
+package job_02;
+
+public class HundredToOne {
+
+
+    public static void main(String args[])
+    {
+        for(int i = 100; i >= 1; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
