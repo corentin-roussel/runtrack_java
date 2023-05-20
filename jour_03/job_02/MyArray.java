5785db0a396984892735eb2052268b515cc2e780
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-20 15:10:11 +0200

    add element to an array and print them

---
+++ jour_03/job_02/MyArray.java
@@ -0,0 +1,13 @@
+package job_02;
+
+class MyArray {
+    public static void main(String Args[])
+    {
+        int[] myArray = {12, 6, 76, 89, 45, 23, 96};
+
+        for(int i = 0; i< myArray.length; i++)
+        {
+            System.out.println(myArray[i]);
+        }
+    }
+}
