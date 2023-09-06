5806e6de51940ace553e512824dd43c5299949be
corentin-roussel <corentin.roussel@laplateforme.io>
2023-09-06 15:17:59 +0200

    job7 calcul d'aire de rectangle et de carré

---
+++ jour1/job7/FormesTest.java
@@ -0,0 +1,31 @@
+package job7;
+
+import java.util.*;
+
+public class FormesTest {
+
+    public int rayonCercle = 5;
+    public int aireCarre = 4;
+
+    public static void main(String[] args)
+    {
+        FormesTest forme = new FormesTest();
+
+        forme.calculCercle();
+        forme.calculCarre();
+    }
+
+    public void calculCercle()
+    {
+        double airCercle = (5 * this.rayonCercle) * 3.14;
+        System.out.println("Aire du cercle de rayon" + this.rayonCercle + ": " + airCercle);
+    }
+
+    public void calculCarre()
+    {
+        double aireCarre = this.aireCarre * this.aireCarre;
+        System.out.println("Aire du cercle de rayon" + this.aireCarre+ ": " + aireCarre);
+    }
+
+
+}
